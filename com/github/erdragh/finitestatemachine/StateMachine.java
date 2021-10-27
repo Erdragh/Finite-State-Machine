666a8c9efc674e07eee9817d36e577842472ffce
Jan <enderjanyt@gmail.com>
2021-10-27 12:38:17 +0200

    Made it an actual state machine library.

--- com/github/erdragh/finitestatemachine/StateMachine.java
+++ com/github/erdragh/finitestatemachine/StateMachine.java
@@ -22,12 +22,8 @@ public class StateMachine {
 
     /**
      * This constructor takes in the information of the language and converts some to ArrayLists for easier use later.
-     * @param alphabet
-     * a char array containing all characters of the language
-     * @param map
-     * a map covering the states another state will switch to when given a certain character
-     * @param finalStates
-     * the states which, if they're the last, mean that the test is positive and the string is part of the language
+     * @param language
+     * This is the language the statemachine will test
      */
     public StateMachine(Language language) {
         int[] finalStatesIn = language.getFinalStates();
@@ -65,15 +61,15 @@ public class StateMachine {
      * the current char that is given to the state machine to check what the new state will be.
      */
     private void setNewState(char character) {
-        Logger.println("Current State: " + state + "; current character: '" + character + "'");
+        // Logger.println("Current State: " + state + "; current character: '" + character + "'");
         int charIndex = alphabet.indexOf(character);
-        Logger.println("Index of Character in alphabet: " + charIndex);
+        // Logger.println("Index of Character in alphabet: " + charIndex);
         if (charIndex == -1) {
             state = -1; 
-            Logger.println("charIndex was -1, therefore not part of the alphabet. Setting state to -1 and returning out of testing");
+            //Logger.println("charIndex was -1, therefore not part of the alphabet. Setting state to -1 and returning out of testing");
             return;
         }
-        Logger.println("New state is: " + map[state][charIndex]);
+        // Logger.println("New state is: " + map[state][charIndex]);
         state = map[state][charIndex];
     }
 }
